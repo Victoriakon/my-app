2ab97a1c059bf5610bc8fa9c8cc4d886fe1df3ed
victoria <larikon@hotmail.com>
2020-09-05 22:00:58 +0300

    Updates

---
+++ precache-manifest.a529aeeb6f66d035a7387e4a516d9558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ae0a6c92109a10481f9e8bd0490de3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f7a928125cba992548e1",
+    "url": "/my-app/static/css/main.5c53236c.chunk.css"
+  },
+  {
+    "revision": "36791cf779a79e8ababc",
+    "url": "/my-app/static/js/2.3a744d3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.3a744d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a928125cba992548e1",
+    "url": "/my-app/static/js/main.2fd1802d.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
